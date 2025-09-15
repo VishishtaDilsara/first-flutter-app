6fa06d3239f4eea8e3822c6bc362a0d7b921361b
VishishtaDilsara <rgvdilsara2002@gmail.com>
2025-09-16 00:42:13 +0530

    Multiple Chile Widget -> Column ->Icon & Image Widget

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -11,14 +11,27 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.greenAccent,
         title: Center(child: Text('Home Page', style: TextStyle(color: Colors.blueGrey, ),),), 
         ),
-      body: Center(child: Text('This is our body', 
-      style: TextStyle(
-        fontSize: 30 ,
-        fontWeight: FontWeight.w900 , 
-        backgroundColor: Colors.limeAccent
-        ), 
-        )
-        ),  
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Text Widget'), 
+            Text('Flutter', style: TextStyle(fontSize: 30, color: Colors.blue, fontWeight: FontWeight.bold),),
+            SizedBox(height: 20),
+            Text('Icon Widget'),
+            Icon(Icons.home, size: 50, color: Colors.amber),
+            SizedBox(height: 20),
+            Text('Image Widget'),
+            Image(
+              image: NetworkImage('https://4.bp.blogspot.com/-JlCmZSSJfAI/UBPllbsjn_I/AAAAAAAAACg/3LiLTDD0h20/s1600/Lotus+flower.jpg') ,
+               height: 200, 
+               width: 200,
+               fit: BoxFit.cover,
+               ),
+            Image.asset('assets/images/thumbnail1.png' , height: 200, width: 200),   
+            ],
+          ),
+      ),  
     );
   }
 }
